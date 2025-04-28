c5b02fec2976945762d67e62f073fbaf342a7e61
Vitalya <vitalikt_22@mail.ru>
2025-04-28 14:10:11 +0400

    feat: implement ts get data func

---
+++ src/funcs/get-data-func.ts
@@ -0,0 +1,12 @@
+export const getData = async (url: string) => {
+	try {
+		const response = await fetch(url)
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`)
+		}
+		const data = await response.json()
+		return data
+	} catch (error) {
+		console.error('Error fetching data:', error)
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { totalPrice } from './funcs/sale-func'
 import { normalizeData } from './funcs/normalize-func'
 import { posts } from './posts'
+import { getData } from './funcs/get-data-func'
+import { COMMENTS_URL } from './contstants'
 
 const price = totalPrice({
 	price: 100000,
@@ -11,3 +13,16 @@ const price = totalPrice({
 
 console.log(price)
 console.log(normalizeData(posts))
+
+interface Comment {
+	id: number
+	email: string
+}
+
+getData(COMMENTS_URL).then((data) => {
+	if (data) {
+		data.forEach((comment: Comment) => {
+			console.log(`ID: ${comment.id}, Email: ${comment.email}`)
+		})
+	}
+})
